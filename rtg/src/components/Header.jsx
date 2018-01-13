400224647eedc0ac072b60fc0e09b27bbe14f0fa
matze09 <matthias@loeks.net>
2018-01-13 22:44:41 +0100

    Replaced kebab menu with classic drawer menu on the top left
    Also removed some boilerplate create-react-app styles from App.scss

--- rtg/src/components/Header.jsx
+++ rtg/src/components/Header.jsx
@@ -1,44 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactRouterProptypes from 'react-router-prop-types';
 import AppBar from 'material-ui/AppBar';
 import { Link, withRouter } from 'react-router-dom';
-import { Divider, IconButton, IconMenu, MenuItem } from 'material-ui';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import { Divider, Drawer, MenuItem } from 'material-ui';
 import ActionSettings from 'material-ui/svg-icons/action/settings';
 import ActionExitToApp from 'material-ui/svg-icons/action/exit-to-app';
 import authService from '../service/AuthService';
 
-const Header = withRouter(({ history }) => (
-  <AppBar
-    title="Royale Tippgemeinschaft - 2018"
-    iconElementRight={authService.isAuthenticated ?
-      <IconMenu
-        iconButtonElement={
-          <IconButton>
-            <MoreVertIcon />
-          </IconButton>
-        }
-      >
-        <Link to="/reception"><MenuItem primaryText="Rezeption" /></Link>
-        <Link to="/bets"><MenuItem primaryText="Tipps" /></Link>
+import './Header.css';
 
-        {authService.isAdmin && <Divider />}
-        {authService.isAdmin &&
-        <Link to="/admin">
-          <MenuItem
-            primaryText="Admin"
-            leftIcon={<ActionSettings />}
-          />
-        </Link>}
+class Header extends Component {
+  constructor(props) {
+    super(props);
 
-        {authService.isAuthenticated && <Divider />}
-        {authService.isAuthenticated && <MenuItem
-          primaryText="Log Out"
-          leftIcon={<ActionExitToApp />}
-          onClick={() => authService.logout(() => history.push('/'))}
-        />}
-      </IconMenu> : null
-    }
-  />
-));
+    this.state = {
+      menuOpen: false,
+    };
 
-export default Header;
+    this.handleMenuToggle = this.handleMenuToggle.bind(this);
+  }
+
+  handleMenuToggle() {
+    this.setState({ menuOpen: !this.state.menuOpen });
+  }
+
+  render() {
+    return (
+      <header className="Header">
+        <AppBar
+          title="Royale Tippgemeinschaft - 2018"
+          onLeftIconButtonClick={this.handleMenuToggle}
+        />
+        <Drawer
+          className="Header__menu"
+          docked={false}
+          open={this.state.menuOpen}
+          onRequestChange={(menuOpen) => this.setState({ menuOpen })}
+        >
+          <Link to="/reception"><MenuItem primaryText="Rezeption" /></Link>
+          <Link to="/bets"><MenuItem primaryText="Tipps" /></Link>
+
+          {authService.isAdmin && <Divider />}
+          {authService.isAdmin &&
+          <Link to="/admin">
+            <MenuItem
+              primaryText="Admin"
+              rightIcon={<ActionSettings />}
+            />
+          </Link>}
+
+          {authService.isAuthenticated && <Divider />}
+          {authService.isAuthenticated && <MenuItem
+            primaryText="Log Out"
+            rightIcon={<ActionExitToApp />}
+            onClick={() => authService.logout(() => this.props.history.push('/'))}
+          />}
+        </Drawer>
+      </header>);
+  }
+}
+
+Header.propTypes = {
+  // eslint-disable-next-line react/no-typos
+  history: ReactRouterProptypes.history.isRequired,
+};
+
+export default withRouter(Header);
