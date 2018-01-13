05a4916ee1ba6349fa28db0b6368b737ab4e9c3b
matze09 <matthias@loeks.net>
2018-01-13 19:12:23 +0100

    Forgot to surround Routes by a Switch component
    Though I don't know yet what its for since it also worked without ;-)

--- rtg/src/App.jsx
+++ rtg/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Admin from './pages/Admin';
 import Bets from './pages/Bets';
 import Foyer from './pages/Foyer';
@@ -10,9 +10,11 @@ import './App.css';
 const App = () => (
   <MuiThemeProvider>
     <div className="App">
-      <Route exact path="/" component={Foyer} />
-      <Route exact path="/admin" component={Admin} />
-      <Route exact path="/bets" component={Bets} />
+      <Switch>
+        <Route exact path="/" component={Foyer} />
+        <Route exact path="/admin" component={Admin} />
+        <Route exact path="/bets" component={Bets} />
+      </Switch>
     </div>
   </MuiThemeProvider>
 );
