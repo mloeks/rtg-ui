a71b4e3f6700b44f8c88b574008fc9f528ff040b
matze09 <matthias@loeks.net>
2018-01-15 19:09:30 +0100

    First version of registration form, still without functionality

--- rtg/src/components/RegisterDialog.jsx
+++ rtg/src/components/RegisterDialog.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Dialog from 'material-ui/Dialog';
-import { FlatButton } from 'material-ui';
+import { Dialog, FlatButton, TextField } from 'material-ui';
 
 class RegisterDialog extends Component {
   constructor(props) {
@@ -15,21 +14,55 @@ class RegisterDialog extends Component {
       lastName: null,
       email: null,
 
-      hasChanges: true,
+      fieldErrors: {
+        username: null,
+        password: null,
+        passwordRepeat: null,
+        firstName: null,
+        lastName: null,
+        email: null,
+      },
+      formError: null,
+      formHasErrors: false,
+
+      hasChanges: false,
     };
+
+    this.updateFormField = this.updateFormField.bind(this);
+    this.updateUsername = this.updateUsername.bind(this);
+    this.updatePassword = this.updatePassword.bind(this);
+    this.updatePasswordRepeat = this.updatePasswordRepeat.bind(this);
+    this.updateFirstName = this.updateFirstName.bind(this);
+    this.updateLastName = this.updateLastName.bind(this);
+    this.updateEmail = this.updateEmail.bind(this);
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  updateFormField(fieldName, newValue) {
+    const newState = { formError: null, formHasErrors: false, hasChanges: true };
+    newState[fieldName] = newValue;
+    this.setState(newState);
+  }
+
+  updateUsername(event, newValue) { this.updateFormField('username', newValue); }
+  updatePassword(event, newValue) { this.updateFormField('password', newValue); }
+  updatePasswordRepeat(event, newValue) { this.updateFormField('passwordRepeat', newValue); }
+  updateFirstName(event, newValue) { this.updateFormField('firstName', newValue); }
+  updateLastName(event, newValue) { this.updateFormField('lastName', newValue); }
+  updateEmail(event, newValue) { this.updateFormField('email', newValue); }
 
   handleSubmit() {
     this.props.onSubmit(this.state.username, this.state.password);
   }
 
+  // validate() {}
+
   render() {
     const actions = [
       <FlatButton label="Abbrechen" secondary onClick={this.props.onCancel} />,
       <FlatButton
-        label="Submit"
+        label="Registrieren"
         primary
         disabled={!this.state.hasChanges}
         onClick={this.handleSubmit}
@@ -38,12 +71,57 @@ class RegisterDialog extends Component {
 
     return (
       <Dialog
-        title="Werde Teil der Royalen Tippgemeinschaft"
+        className="RegisterDialog"
         actions={actions}
+        autoScrollBodyContent
         modal
         open={this.props.open}
+        title="Werde Teil der Royalen Tippgemeinschaft"
       >
-        Only actions can close this dialog.
+        <TextField
+          errorText={this.state.fieldErrors.username || false}
+          floatingLabelText="Username"
+          fullWidth
+          onChange={this.updateUsername}
+        />
+        <br />
+        <TextField
+          errorText={this.state.fieldErrors.email || false}
+          floatingLabelText="E-Mail"
+          fullWidth
+          onChange={this.updateEmail}
+        />
+        <br />
+        <TextField
+          errorText={this.state.fieldErrors.password || false}
+          floatingLabelText="Passwort"
+          fullWidth
+          type="password"
+          onChange={this.updatePassword}
+        />
+        <br />
+        <TextField
+          errorText={this.state.fieldErrors.passwordRepeat || false}
+          floatingLabelText="Passwort wiederholen"
+          fullWidth
+          type="password"
+          onChange={this.updatePasswordRepeat}
+        />
+        <br />
+        <div style={{ display: 'flex' }}>
+          <TextField
+            style={{ marginRight: '10px' }}
+            errorText={this.state.fieldErrors.firstName || false}
+            floatingLabelText="Vorname"
+            onChange={this.updateFirstName}
+          />
+          <TextField
+            style={{ marginLeft: '10px' }}
+            errorText={this.state.fieldErrors.lastName || false}
+            floatingLabelText="Nachname"
+            onChange={this.updateLastName}
+          />
+        </div>
       </Dialog>
     );
   }
