10ce468c31a0821a3367de36fd1874f13b02181d
matze09 <matthias@loeks.net>
2018-01-24 11:03:39 +0100

    Some styling adjustments to the user menu

--- rtg/src/components/UserMenu.jsx
+++ rtg/src/components/UserMenu.jsx
@@ -6,9 +6,8 @@ import muiThemeable from 'material-ui/styles/muiThemeable';
 import { Avatar, Divider, FlatButton, IconMenu, MenuItem } from 'material-ui';
 import ActionSettings from 'material-ui/svg-icons/action/settings';
 import ActionExitToApp from 'material-ui/svg-icons/action/exit-to-app';
+import SocialPerson from 'material-ui/svg-icons/social/person';
 import AuthService from '../service/AuthService';
-
-import './UserMenu.css';
 
 const UserMenu = (props) => {
   const userAndAvatar = (
@@ -21,24 +20,33 @@ const UserMenu = (props) => {
           className="UserMenu__avatar"
           color={props.muiTheme.appBar.avatarColor}
           backgroundColor={props.muiTheme.appBar.avatarBackgroundColor}
-          size={25}
+          size={35}
         >{props.username[0].toUpperCase()}
         </Avatar>
       }
+      style={{ height: '50px' }}
     />
   );
+
+  const linkStyle = { textDecoration: 'none' };
 
   return (
     <IconMenu
       iconButtonElement={userAndAvatar}
       anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
       targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+      menuStyle={{ textAlign: 'left' }}
     >
-      <Link to="/profile"><MenuItem primaryText="Profil" /></Link>
+      <Link to="/profile" style={linkStyle}>
+        <MenuItem
+          primaryText="Profil"
+          rightIcon={<SocialPerson />}
+        />
+      </Link>
 
       {AuthService.isAdmin() && <Divider />}
       {AuthService.isAdmin() &&
-      <Link to="/admin">
+      <Link to="/admin" style={linkStyle}>
         <MenuItem
           primaryText="Admin"
           rightIcon={<ActionSettings />}
