38794b72f88f90403c184f1437c1ebcd23c24124
matze09 <matthias@loeks.net>
2018-03-17 01:52:11 +0100

    Add TODO in order not to forget where the icons where taken from

--- rtg/src/pages/Imprint.jsx
+++ rtg/src/pages/Imprint.jsx
@@ -4,6 +4,7 @@ import Page from './Page';
 const Imprint = () => (
   <Page className="ImprintPage">
     <h3>Impressum</h3>
+    {/* Icons von hier: https://www.shareicon.net/pack/flat-round-world-flag-icon-set */}
   </Page>
 );
 
