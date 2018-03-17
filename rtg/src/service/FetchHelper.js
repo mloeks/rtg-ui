c8f36799fa9d04aa4d7c561f134edfb91ecfe3f7
matze09 <matthias@loeks.net>
2018-03-17 18:19:20 +0100

    Make FetchHelper be able to deal with empty response bodies on 204

--- rtg/src/service/FetchHelper.js
+++ rtg/src/service/FetchHelper.js
@@ -1,5 +1,13 @@
 class FetchHelper {
   static parseJson(response) {
+    if (response.status === 204) {
+      return new Promise(resolve => (resolve({
+        status: response.status,
+        ok: response.ok,
+        json: null,
+      })));
+    }
+
     return new Promise(resolve => response.json()
       .then(json => resolve({
         status: response.status,
