42dfa4ac61acad8367c58f99d9c85f597e25557d
matze09 <matthias@loeks.net>
2018-03-24 00:29:45 +0100

    Add todo for avatar upload

--- rtg/src/pages/Profile.jsx
+++ rtg/src/pages/Profile.jsx
@@ -7,6 +7,9 @@ import AuthService from '../service/AuthService';
 
 import headingImg from '../theme/img/img8.jpg';
 
+// TODO P1 handle avatar in the state of this component and pass it down to
+// BigEditableAvatar and ProfileForm as prop. Have it updated by BigEditableAvatar callback.
+
 const Profile = () => (
   <Page className="ProfilePage">
     <BigPicture className="ProfilePage__heading" img={headingImg} />
