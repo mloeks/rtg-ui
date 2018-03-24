f4ff4205c5651daa0e2fe760e68e6bb08b50dc8a
matze09 <matthias@loeks.net>
2018-03-24 16:43:49 +0100

    Start handling avatar cropping

--- rtg/src/components/profile/BigEditableAvatar.jsx
+++ rtg/src/components/profile/BigEditableAvatar.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Avatar } from 'material-ui';
 import { teal400 } from 'material-ui/styles/colors';
 import Person from 'material-ui/svg-icons/social/person';
+import AvatarImageCropper from 'react-avatar-image-cropper';
 
 import './BigEditableAvatar.css';
 
@@ -10,18 +11,45 @@ import './BigEditableAvatar.css';
 class BigEditableAvatar extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      editing: false,
+    };
+    this.avatarCropApply = this.avatarCropApply.bind(this);
+    this.handleAvatarClick = this.handleAvatarClick.bind(this);
+  }
+
+  avatarCropApply(file) {}
+
+  handleAvatarClick(e) {
+    console.log('click');
+    this.setState({ editing: true }, () => {
+      // document.querySelector('.BigEditableAvatar__cropper input').click();
+    });
+    e.preventDefault();
   }
 
   render() {
+    const avatarCropper = (<AvatarImageCropper
+      className="BigEditableAvatar__cropper"
+      apply={this.avatarCropApply}
+      iconStyle={{ display: 'none' }}
+      textStyle={{ display: 'none' }}
+      rootStyle={this.state.editing ? { width: '200px' } : { width: 0 }}
+      maxsize={1024 * 1024 * 5}
+    />);
+
     return (
       <div className="BigEditableAvatar">
         <Avatar
           className="BigEditableAvatar__avatar"
           backgroundColor={!this.props.avatarUrl ? teal400 : null}
-          icon={!this.props.avatarUrl ? <Person /> : null}
+          icon={!this.props.avatarUrl ? <Person style={{ pointerEvents: 'none' }} /> : null}
           src={this.props.avatarUrl}
           size={150}
-        />
+          onClick={this.handleAvatarClick}
+        >{avatarCropper}
+        </Avatar>
         <h2 className="BigEditableAvatar__username">{this.props.username}</h2>
       </div>
     );
