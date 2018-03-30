1fca0599b8b54eced1eadfe828bed759f5b3d7ac
matze09 <matthias@loeks.net>
2018-03-31 00:20:33 +0200

    Add first version of very simple deployment script
    It builds the bundles and uploads the assets
    Paths are not environment-aware yet

---
+++ deployment/deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+ENV=$1
+SSH=muden@s17.wservices.ch
+
+cd ../rtg
+
+echo "Preparing NPM..."
+/home/mloeks/.nvm/versions/node/v8.9.4/bin/npm run clean
+#/home/mloeks/.nvm/versions/node/v8.9.4/bin/npm i
+#/home/mloeks/.nvm/versions/node/v8.9.4/bin/npm rebuild node-sass --force
+
+echo "Building ${ENV} bundles..."
+/home/mloeks/.nvm/versions/node/v8.9.4/bin/npm run build
+
+echo "Uploading new assets"
+TARGET_APP=$([ "$ENV" == "PROD" ] && echo "rtg" || echo "rtg_demo")
+ssh ${SSH} rm -rf ~/${TARGET_APP}/rtg_frontend/*
+scp -r build/* ${SSH}:~/${TARGET_APP}/rtg_frontend
+
+
+
+
+
+
