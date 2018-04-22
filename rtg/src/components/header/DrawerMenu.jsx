4aeca6ba5f55af78d5db48fbf4fbc1d3ee3e14df
matze09 <matthias@loeks.net>
2018-04-23 00:03:11 +0200

    Introduce Google style profile card on top of Drawer menu :)

--- rtg/src/components/header/DrawerMenu.jsx
+++ rtg/src/components/header/DrawerMenu.jsx
@@ -5,10 +5,10 @@ import ActionSettings from 'material-ui/svg-icons/action/settings';
 import ActionExitToApp from 'material-ui/svg-icons/action/exit-to-app';
 import Home from 'material-ui/svg-icons/action/home';
 import List from 'material-ui/svg-icons/action/list';
-import SocialPerson from 'material-ui/svg-icons/social/person';
 import Today from 'material-ui/svg-icons/action/today';
 import TrendingUp from 'material-ui/svg-icons/action/trending-up';
 import { Link } from 'react-router-dom';
+import ProfileCard from './ProfileCard';
 import AuthService from '../../service/AuthService';
 
 import './DrawerMenu.css';
@@ -35,7 +35,9 @@ const DrawerMenu = (props) => {
       open={props.open}
       onRequestChange={props.onRequestChange}
     >
-      <Link to="/profile"><MenuItem primaryText="Profil" leftIcon={<SocialPerson />} /></Link>
+      <Link to="/profile">
+        <ProfileCard username={AuthService.getUsername()} avatar={AuthService.getAvatar()} />
+      </Link>
 
       <Divider />
 
---
+++ rtg/src/components/header/ProfileCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import UserAvatar from '../UserAvatar';
+
+import profileBgImage from '../../theme/img/img9.jpg';
+import './ProfileCard.css';
+
+const ProfileCard = props => (
+  <div
+    className="ProfileCard"
+    style={{ backgroundImage: `url(${profileBgImage})` }}
+  >
+    <UserAvatar
+      size={65}
+      img={props.avatar}
+      username={props.username}
+      style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0 0 3px, rgba(0, 0, 0, 0.15) 2px 3px 5px' }}
+    />
+    <div className="ProfileCard__content">
+      <div className="ProfileCard__username">{props.username}</div>
+      <div className="ProfileCard__subtitle">Hier klicken für persönliche Einstellungen</div>
+    </div>
+  </div>
+
+);
+
+ProfileCard.defaultProps = {
+  avatar: null,
+};
+
+ProfileCard.propTypes = {
+  avatar: PropTypes.string,
+  username: PropTypes.string.isRequired,
+};
+
+export default ProfileCard;
