b30e3ef9dab10f873478748af2bd3e3aa155ad69
matze09 <matthias@loeks.net>
2018-05-19 19:21:58 +0200

    Handle AJAX 401 responses
    Redirect the user to the homepage if he is not authenticated anymore
    during an AJAX request.

--- rtg/src/service/FetchHelper.js
+++ rtg/src/service/FetchHelper.js
@@ -2,6 +2,11 @@ class FetchHelper {
   static parseJson(response) {
     const contentType = response.headers.get('content-type');
 
+    // redirect to login page if user is not logged in anymore
+    if (response.status === 401) {
+      window.location.href = '/';
+    }
+
     if (contentType && contentType.indexOf('application/json') !== -1) {
       return new Promise(resolve => response.json()
         .then(json => resolve({
