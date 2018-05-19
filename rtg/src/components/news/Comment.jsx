59ff56342be0a12938933821fbaf1eb5e8b8ea11
matze09 <matthias@loeks.net>
2018-05-19 19:42:14 +0200

    Show UserDetailsPopover also when clicking on comment user avatars

--- rtg/src/components/news/Comment.jsx
+++ rtg/src/components/news/Comment.jsx
@@ -5,18 +5,35 @@ import CommentsList from './CommentsList';
 import { lightGrey } from '../../theme/RtgTheme';
 import UserAvatar from '../UserAvatar';
 import { getFormattedPostDate } from './Post';
+import UserDetailsPopover from '../standings/UserDetailsPopover';
 
 import './Comment.css';
 
 export const MAX_REPLY_DEPTH = 4;
 
-// TODO P1 also show user details popover on avatar click
 class Comment extends Component {
   constructor(props) {
     super(props);
-    this.state = { showAddComment: false };
+
+    this.state = {
+      showAddComment: false,
+      userDetailsPopoverAnchorEl: null,
+      userDetailsPopoverOpen: false,
+    };
+
     this.toggleAddReply = this.toggleAddReply.bind(this);
     this.handleReplyAdded = this.handleReplyAdded.bind(this);
+    this.showUserDetailsPopover = this.showUserDetailsPopover.bind(this);
+    this.hideUserDetailsPopover = this.hideUserDetailsPopover.bind(this);
+  }
+
+  showUserDetailsPopover(e) {
+    e.preventDefault();
+    this.setState({ userDetailsPopoverOpen: true, userDetailsPopoverAnchorEl: e.currentTarget });
+  }
+
+  hideUserDetailsPopover() {
+    this.setState({ userDetailsPopoverOpen: false });
   }
 
   toggleAddReply() {
@@ -32,16 +49,26 @@ class Comment extends Component {
       <div className="Comment">
         <div className="Comment__avatar-content-wrapper">
           <div className="Comment__avatar">
+            <UserDetailsPopover
+              anchorEl={this.state.userDetailsPopoverAnchorEl}
+              avatar={this.props.comment.author_details.avatar}
+              userId={this.props.comment.author_details.pk}
+              username={this.props.comment.author_details.username}
+              open={this.state.userDetailsPopoverOpen}
+              onClose={this.hideUserDetailsPopover}
+            />
+
             <UserAvatar
               size={40}
               username={this.props.comment.author_details.username}
               img={this.props.comment.author_details.avatar}
+              onClick={this.showUserDetailsPopover}
             />
           </div>
           <div>
             <div className="Comment__author-date">
               <span className="Comment__author">
-                {this.props.comment.author_details.username} –&nbsp;
+                {this.props.comment.author_details.username}&nbsp;–&nbsp;
               </span>
               <span>{getFormattedPostDate(this.props.comment.date_created)}</span>
             </div>
