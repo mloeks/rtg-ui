128c908c407a16f39ba4b33ffe1be0cd7b7f38c9
matze09 <matthias@loeks.net>
2018-05-20 12:31:25 +0200

    Removed viewport helper method which can now be achieved with verge

--- rtg/src/service/EventsHelper.js
+++ rtg/src/service/EventsHelper.js
@@ -93,13 +93,3 @@ export class ThrottledScrollPositionListener {
     window.removeEventListener('scroll', this._listener);
   }
 }
-
-export const isInViewport = (element) => {
-  const rect = element.getBoundingClientRect();
-  const html = document.documentElement;
-  return (
-    rect.top >= 0 && rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || html.clientHeight) &&
-    rect.right <= (window.innerWidth || html.clientWidth)
-  );
-};
