bc9db3d8df3e5f43dbb2fb3b8dece5af301f643f
matze09 <matthias@loeks.net>
2018-05-20 12:23:52 +0200

    Add helper functions for event handling and - in particular - resource efficient scroll event handling.

---
+++ rtg/src/service/EventsHelper.js
@@ -0,0 +1,105 @@
+// throttle event listeners using requestAnimationFrames
+// Adopted from https://developer.mozilla.org/en-US/docs/Web/Events/resize
+// Made an ES6 class from the code, made generic for arbitrary event, added remove method
+export class ThrottledEventListener {
+  constructor(event) {
+    this.event = event;
+    this.callbacks = [];
+    this.running = false;
+
+    this._runCallbacks = this._runCallbacks.bind(this);
+    this._listener = this._listener.bind(this);
+    this._addCallback = this._addCallback.bind(this);
+  }
+
+  _runCallbacks() {
+    this.callbacks.forEach((callback) => { callback(); });
+    this.running = false;
+  }
+
+  // fired on event
+  _listener() {
+    if (!this.running) {
+      this.running = true;
+
+      if (window.requestAnimationFrame) {
+        window.requestAnimationFrame(this._runCallbacks);
+      } else {
+        setTimeout(this._runCallbacks, 66);
+      }
+    }
+  }
+
+  _addCallback(callback) {
+    if (callback) {
+      this.callbacks.push(callback);
+    }
+  }
+
+  addCallback(callback) {
+    if (!this.callbacks.length) {
+      window.addEventListener(this.event, this._listener);
+    }
+    this._addCallback(callback);
+  }
+
+  removeAll() {
+    window.removeEventListener(this.event, this._listener);
+  }
+}
+
+// Adopted from https://developer.mozilla.org/en-US/docs/Web/Events/scroll
+export class ThrottledScrollPositionListener {
+  constructor() {
+    this.callbacks = [];
+    this.lastKnownScrollPosition = 0;
+    this.ticking = false;
+
+    this._runCallbacks = this._runCallbacks.bind(this);
+    this._listener = this._listener.bind(this);
+    this._addCallback = this._addCallback.bind(this);
+  }
+
+  _runCallbacks() {
+    this.callbacks.forEach((callback) => { callback(this.lastKnownScrollPosition); });
+    this.ticking = false;
+  }
+
+  _listener() {
+    // Note that IE does not support scrollY
+    this.lastKnownScrollPosition =
+      typeof window.scrollY === 'undefined' ? window.pageYOffset : window.scrollY;
+
+    if (!this.ticking) {
+      window.requestAnimationFrame(this._runCallbacks);
+      this.ticking = true;
+    }
+  }
+
+  _addCallback(callback) {
+    if (callback) {
+      this.callbacks.push(callback);
+    }
+  }
+
+  addCallback(callback) {
+    if (!this.callbacks.length) {
+      window.addEventListener('scroll', this._listener);
+    }
+    this._addCallback(callback);
+  }
+
+  removeAll() {
+    window.removeEventListener('scroll', this._listener);
+  }
+}
+
+export const isInViewport = (element) => {
+  const rect = element.getBoundingClientRect();
+  const html = document.documentElement;
+  return (
+    rect.top >= 0 && rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  );
+};
