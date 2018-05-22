7fc1899a2cf264d1df74a4e330270f1a07f3748d
matze09 <matthias@loeks.net>
2018-05-22 22:16:58 +0200

    Add delete confirmation dialog

--- rtg/src/components/admin/UserCard.jsx
+++ rtg/src/components/admin/UserCard.jsx
@@ -1,6 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardActions, CardMedia, CardText, CardTitle, CircularProgress, IconButton } from 'material-ui';
+import {
+  Card,
+  CardActions,
+  CardMedia,
+  CardText,
+  CardTitle,
+  CircularProgress,
+  Dialog,
+  FlatButton,
+  IconButton,
+} from 'material-ui';
 import ActionDelete from 'material-ui/svg-icons/action/delete';
 import ActionEuroSymbol from 'material-ui/svg-icons/action/euro-symbol';
 import AlertWarning from 'material-ui/svg-icons/alert/warning';
@@ -8,19 +18,48 @@ import Person from 'material-ui/svg-icons/social/person';
 import { teal400 } from 'material-ui/styles/colors';
 import { API_BASE_URL } from '../../service/AuthService';
 import { darkGold, darkGrey, error, gold, grey, lightGrey } from '../../theme/RtgTheme';
-import { lightenDarkenColor } from "../../service/ColorHelper";
+import { lightenDarkenColor } from '../../service/ColorHelper';
 
 const CARD_SIZE = 180;
+
+const DeleteConfirmationModal = (props) => {
+  const actions = [
+    <FlatButton label="Abbrechen" onClick={props.onCancel} />,
+    <FlatButton label="User löschen" primary onClick={props.onConfirm} />,
+  ];
+
+  return (
+    <Dialog
+      actions={actions}
+      modal
+      open={props.open}
+      title="Löschen bestätigen"
+    >
+      &quot;{props.username}&quot; wirklich endgültig löschen?<br />
+      Diese Aktion kann nicht rückgängig gemacht werden!
+    </Dialog>
+  );
+};
+
+DeleteConfirmationModal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  username: PropTypes.string.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+};
 
 class UserCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      deleteConfirmationModalOpen: false,
       savingInProgress: false,
       savingIssues: false,
     };
     this.toggleHasPaid = this.toggleHasPaid.bind(this);
     this.handleDeleteRequest = this.handleDeleteRequest.bind(this);
+    this.handleDeleteRequestCancelled = this.handleDeleteRequestCancelled.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   toggleHasPaid() {
@@ -28,7 +67,15 @@ class UserCard extends Component {
   }
 
   handleDeleteRequest() {
-    // TODO show confirmation
+    this.setState({ deleteConfirmationModalOpen: true });
+  }
+
+  handleDeleteRequestCancelled() {
+    this.setState({ deleteConfirmationModalOpen: false });
+  }
+
+  handleDelete() {
+    this.setState({ deleteConfirmationModalOpen: false });
     this.props.onDelete(this.props.pk);
   }
 
@@ -62,87 +109,95 @@ class UserCard extends Component {
     );
 
     return (
-      <Card
-        style={{
-          width: CARD_SIZE,
-          textAlign: 'left',
-          margin: '5px',
-        }}
-        containerStyle={{ display: 'flex', flexDirection: 'column', height: '100%' }}
-      >
-        <CardMedia
-          overlay={
-            <CardTitle
-              title={this.props.username}
-              style={{ padding: '10px'}}
-              titleStyle={{
-                color: 'white',
-                textShadow: 'black 1px 1px 4px',
-                fontFamily: '"Lobster Two", sans-serif',
-                fontSize: '20px',
-                lineHeight: '24px',
-              }}
-            />
-          }
-          style={{ height: 0.7 * CARD_SIZE, overflow: 'hidden', opacity: isInactive ? 0.3 : 1 }}
-          mediaStyle={{ height: '100%', marginTop: this.props.avatar ? '-15%' : '0' }}
-          overlayContentStyle={{ background: 'rgba(0, 0, 0, 0.35)' }}
-        >
-          {this.props.avatar && <img src={`${API_BASE_URL}/media/${this.props.avatar}`} alt="User avatar" />}
-          {!this.props.avatar && noAvatarPlaceholder}
-        </CardMedia>
-        <CardText
+      <Fragment>
+        <DeleteConfirmationModal
+          open={this.state.deleteConfirmationModalOpen}
+          username={this.props.username}
+          onCancel={this.handleDeleteRequestCancelled}
+          onConfirm={this.handleDelete}
+        />
+        <Card
           style={{
-            fontSize: '16px',
-            flexGrow: 1,
-            padding: '10px',
-            opacity: isInactive ? 0.3 : 1,
+            width: CARD_SIZE,
+            textAlign: 'left',
+            margin: '5px',
           }}
+          containerStyle={{ display: 'flex', flexDirection: 'column', height: '100%' }}
         >
-          <span style={{ fontWeight: 500, wordBreak: 'break-word' }}>
-            {`${this.props.first_name} ${this.props.last_name}`}
-          </span><br />
-          <div style={{
-            color: grey,
-            fontSize: '14px',
-            fontWeight: 400,
-            wordBreak: 'break-word',
-          }}
-          ><span>{this.props.email}</span><br />
-            {this.props.email2 && <span>{this.props.email2}</span>}
-          </div>
-        </CardText>
+          <CardMedia
+            overlay={
+              <CardTitle
+                title={this.props.username}
+                style={{ padding: '10px'}}
+                titleStyle={{
+                  color: 'white',
+                  textShadow: 'black 1px 1px 4px',
+                  fontFamily: '"Lobster Two", sans-serif',
+                  fontSize: '20px',
+                  lineHeight: '24px',
+                }}
+              />
+            }
+            style={{ height: 0.7 * CARD_SIZE, overflow: 'hidden', opacity: isInactive ? 0.3 : 1 }}
+            mediaStyle={{ height: '100%', marginTop: this.props.avatar ? '-15%' : '0' }}
+            overlayContentStyle={{ background: 'rgba(0, 0, 0, 0.35)' }}
+          >
+            {this.props.avatar && <img src={`${API_BASE_URL}/media/${this.props.avatar}`} alt="User avatar" />}
+            {!this.props.avatar && noAvatarPlaceholder}
+          </CardMedia>
+          <CardText
+            style={{
+              fontSize: '16px',
+              flexGrow: 1,
+              padding: '10px',
+              opacity: isInactive ? 0.3 : 1,
+            }}
+          >
+            <span style={{ fontWeight: 500, wordBreak: 'break-word' }}>
+              {`${this.props.first_name} ${this.props.last_name}`}
+            </span><br />
+            <div style={{
+              color: grey,
+              fontSize: '14px',
+              fontWeight: 400,
+              wordBreak: 'break-word',
+            }}
+            ><span>{this.props.email}</span><br />
+              {this.props.email2 && <span>{this.props.email2}</span>}
+            </div>
+          </CardText>
 
-        <CardActions
-          style={{ display: 'flex', justifyContent: isInactive ? 'flex-end' : 'space-between' }}
-        >
-          {!isInactive &&
+          <CardActions
+            style={{ display: 'flex', justifyContent: isInactive ? 'flex-end' : 'space-between' }}
+          >
+            {!isInactive &&
+              <IconButton
+                title={`Als ${this.props.has_paid ? 'unbezahlt' : 'bezahlt'} markieren`}
+                onClick={this.toggleHasPaid}
+                style={iconButtonStyle}
+                iconStyle={{ margin: 0 }}
+              >{paidSymbol}
+              </IconButton>}
+
+            {this.state.savingInProgress && <CircularProgress size={20} thickness={2} />}
+            {this.state.savingIssues &&
+              <IconButton
+                title="Fehler beim Speichern, bitte neu laden."
+                style={iconButtonStyle}
+                iconStyle={{ margin: 0 }}
+              ><AlertWarning color={error} />
+              </IconButton>}
+
             <IconButton
-              title={`Als ${this.props.has_paid ? 'unbezahlt' : 'bezahlt'} markieren`}
-              onClick={this.toggleHasPaid}
+              title="Benutzer löschen"
+              onClick={this.handleDeleteRequest}
               style={iconButtonStyle}
               iconStyle={{ margin: 0 }}
-            >{paidSymbol}
-            </IconButton>}
-
-          {this.state.savingInProgress && <CircularProgress size={20} thickness={2} />}
-          {this.state.savingIssues &&
-            <IconButton
-              title="Fehler beim Speichern, bitte neu laden."
-              style={iconButtonStyle}
-              iconStyle={{ margin: 0 }}
-            ><AlertWarning color={error} />
-            </IconButton>}
-
-          <IconButton
-            title="Benutzer löschen"
-            onClick={this.handleDeleteRequest}
-            style={iconButtonStyle}
-            iconStyle={{ margin: 0 }}
-          ><ActionDelete color={grey} hoverColor={darkGrey} />
-          </IconButton>
-        </CardActions>
-      </Card>
+            ><ActionDelete color={grey} hoverColor={darkGrey} />
+            </IconButton>
+          </CardActions>
+        </Card>
+      </Fragment>
     );
   }
 }
