01e343ddc6b857fd993b2c14d53981966d3077d1
matze09 <matthias@loeks.net>
2018-05-24 00:01:30 +0200

    Add debounce function

--- rtg/src/service/EventsHelper.js
+++ rtg/src/service/EventsHelper.js
@@ -104,3 +104,25 @@ export class ThrottledScrollPositionListener {
 }
 
 export const hasScrolledBehind = elem => rectangle(elem).bottom < viewportH();
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+// Source: https://davidwalsh.name/javascript-debounce-function
+/* eslint-disable */
+export function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+/* eslint-enable */
