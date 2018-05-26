9ee37bff13926ad3b7956736a669410d0e999838
matze09 <matthias@loeks.net>
2018-05-26 20:06:26 +0200

    Get standings excerpt right and do some performance improvements
    * Only add rows to the DOM which should be displayed
    * Only calculate displayRank once on mount, not on every render

--- rtg/src/components/standings/StandingsTable.jsx
+++ rtg/src/components/standings/StandingsTable.jsx
@@ -32,6 +32,7 @@ export const pointsColumnStyle = {
 
 // TODO P3 FEATURE Prio 3 alle bets in Tabelle anzeigen (scrollbar) (a la Kicktipp / Doodle)
 class StandingsTable extends Component {
+  // TODO P2 take noBets into account?
   static identicalRank(row, lastRow) {
     return row && lastRow &&
       row.points === lastRow.points && row.noVolltreffer === lastRow.noVolltreffer;
@@ -40,18 +41,20 @@ class StandingsTable extends Component {
   static statsToStateMapper(stats) {
     return {
       loadingError: false,
-      rows: stats.map(statObj => ({
-        userId: statObj.user,
-        username: statObj.username,
-        userAvatar: statObj.user_avatar,
-        points: statObj.points,
-        noBets: statObj.no_bets,
-        noVolltreffer: statObj.no_volltreffer,
-        noDifferenz: statObj.no_differenz,
-        noRemisTendenz: statObj.no_remis_tendenz,
-        noTendenz: statObj.no_tendenz,
-        noNiete: statObj.no_niete,
-      })),
+      rows: StandingsTable.enhanceRowsWithDisplayRanks(
+        stats.map(statObj => ({
+          userId: statObj.user,
+          username: statObj.username,
+          userAvatar: statObj.user_avatar,
+          points: statObj.points,
+          noBets: statObj.no_bets,
+          noVolltreffer: statObj.no_volltreffer,
+          noDifferenz: statObj.no_differenz,
+          noRemisTendenz: statObj.no_remis_tendenz,
+          noTendenz: statObj.no_tendenz,
+          noNiete: statObj.no_niete,
+        })),
+      ),
     };
   }
 
@@ -59,27 +62,37 @@ class StandingsTable extends Component {
   static usersOnlyToStateMapper(users) {
     return {
       loadingError: false,
-      rows: users.map(user => ({
-        userId: user.pk,
-        username: user.username,
-        userAvatar: user.avatar,
-        points: 0,
-        noBets: 0,
-        noVolltreffer: 0,
-        noDifferenz: 0,
-        noRemisTendenz: 0,
-        noTendenz: 0,
-        noNiete: 0,
-      })),
+      rows: StandingsTable.enhanceRowsWithDisplayRanks(
+        users.map(user => ({
+          userId: user.pk,
+          username: user.username,
+          userAvatar: user.avatar,
+          points: 0,
+          noBets: 0,
+          noVolltreffer: 0,
+          noDifferenz: 0,
+          noRemisTendenz: 0,
+          noTendenz: 0,
+          noNiete: 0,
+        })),
+      ),
     };
+  }
+
+  static enhanceRowsWithDisplayRanks(rows) {
+    let lastRow = null;
+    return rows.map((row, ix) => {
+      const rankEnhancedRow = Object.assign({}, row);
+      rankEnhancedRow.displayRank =
+        StandingsTable.identicalRank(row, lastRow) ? '' : (ix + 1).toString();
+      lastRow = row;
+      return rankEnhancedRow;
+    });
   }
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      rows: [],
-    };
+    this.state = { rows: [] };
   }
 
   componentDidMount() {
@@ -95,6 +108,33 @@ class StandingsTable extends Component {
     }).catch(() => this.setState({ loading: false, loadingError: true }));
   }
 
+  getDisplayedRows(rows, classList) {
+    if (this.props.showOnlyUserExcerpt) {
+      classList.push('StandingsTable--excerpt');
+
+      const userRank = rows.findIndex(r => r.userId === AuthService.getUserId());
+      const numberUsersAroundUserOneSide = Math.floor(this.props.userExcerptRows / 2);
+      const fromIndex = userRank - numberUsersAroundUserOneSide;
+      if (fromIndex <= 0) {
+        // top end
+        classList.push('StandingsTable--excerpt-bottom-fade');
+        return rows.slice(0, this.props.userExcerptRows);
+      }
+
+      const toIndex = userRank + numberUsersAroundUserOneSide;
+      if (toIndex >= rows.length - 1) {
+        // bottom end
+        classList.push('StandingsTable--excerpt-top-fade');
+        return rows.slice(-this.props.userExcerptRows);
+      }
+
+      classList.push('StandingsTable--excerpt-top-fade');
+      classList.push('StandingsTable--excerpt-bottom-fade');
+      return rows.slice(fromIndex, toIndex + 1);
+    }
+    return rows;
+  }
+
   fetchUsersOnly() {
     fetch(`${API_BASE_URL}/rtg/users_public/`,
       { headers: { Authorization: `Token ${AuthService.getToken()}` } },
@@ -107,41 +147,12 @@ class StandingsTable extends Component {
     )).catch(() => this.setState({ loading: false, loadingError: true }));
   }
 
-  getExcerptUserScrollTopStyle(totalUsers, userRank) {
-    if (userRank === 0) {
-      return { top: 0 };
-    }
-    if (userRank === totalUsers - 1) {
-      return { bottom: 0 };
-    }
-
-    return { top: -0.5 * (userRank * this.props.rowHeight) };
-  }
-
   render() {
-    let lastRow = null;
-    const totalUsers = this.state.rows.length;
-    const userRank = this.state.rows.findIndex(r => r.userId === AuthService.getUserId());
-    const excerptHeight = this.props.userExcerptRows * this.props.rowHeight;
-    const wrapperStyle = this.props.showOnlyUserExcerpt ?
-      { position: 'absolute', ...this.getExcerptUserScrollTopStyle(totalUsers, userRank) } : {};
-
     const standingsTableClassList = ['StandingsTable'];
-    if (this.props.showOnlyUserExcerpt) {
-      standingsTableClassList.push('StandingsTable--excerpt');
-      if (userRank !== 0) {
-        standingsTableClassList.push('StandingsTable--excerpt-top-fade');
-      }
-      if (userRank !== totalUsers - 1) {
-        standingsTableClassList.push('StandingsTable--excerpt-bottom-fade');
-      }
-    }
+    const displayedRows = this.getDisplayedRows(this.state.rows, standingsTableClassList);
 
     return (
-      <div
-        className={standingsTableClassList.join(' ')}
-        style={this.props.showOnlyUserExcerpt ? { height: excerptHeight } : {}}
-      >
+      <div className={standingsTableClassList.join(' ')}>
         {this.state.loading && <CircularProgress className="StandingsTable__loading-spinner" />}
         {this.state.loadingError &&
           <Notification
@@ -152,11 +163,7 @@ class StandingsTable extends Component {
           />}
 
         {(!this.state.loading && !this.state.loadingError) &&
-          <Table
-            className="StandingsTable__table"
-            selectable={false}
-            wrapperStyle={wrapperStyle}
-          >
+          <Table className="StandingsTable__table" selectable={false}>
             {this.props.showTableHeader &&
               <TableHeader
                 displaySelectAll={false}
@@ -198,19 +205,14 @@ class StandingsTable extends Component {
               </TableHeader>}
 
             <TableBody showRowHover displayRowCheckbox={false}>
-              {this.state.rows.map((row, ix) => {
-                const displayRank =
-                  StandingsTable.identicalRank(row, lastRow) ? '' : (ix + 1).toString();
-                lastRow = row;
-                  return (<StandingsTableRow
-                    key={row.userId}
-                    rank={displayRank}
-                    rowHeight={this.props.rowHeight}
-                    self={AuthService.getUserId() === row.userId}
-                    showStatsColumns={this.props.showStatsColumns}
-                    {...row}
-                  />);
-                })}
+              {displayedRows.map(row => (<StandingsTableRow
+                key={row.userId}
+                rank={row.displayRank}
+                rowHeight={this.props.rowHeight}
+                self={AuthService.getUserId() === row.userId}
+                showStatsColumns={this.props.showStatsColumns}
+                {...row}
+              />))}
             </TableBody>
           </Table>}
       </div>
