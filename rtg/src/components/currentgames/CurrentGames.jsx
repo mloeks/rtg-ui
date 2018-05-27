fc553f006600d2cc2e6fe28b5496ef83990abe80
matze09 <matthias@loeks.net>
2018-05-27 16:50:59 +0200

    Start handling loading of games and display of visible excerpt correctly

--- rtg/src/components/currentgames/CurrentGames.jsx
+++ rtg/src/components/currentgames/CurrentGames.jsx
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
-import { format, parse } from 'date-fns';
+import { parse } from 'date-fns';
 import FetchHelper from '../../service/FetchHelper';
 import AuthService, { API_BASE_URL } from '../../service/AuthService';
 import GameCard from '../GameCard';
 import GameCardGameInfo from '../GameCardGameInfo';
 
 class CurrentGames extends Component {
+  static range(start, end) {
+    return Array.from({ length: (end - start) }, (v, k) => k + start);
+  }
+
+  static getInitialOffsetBasedOnDate(kickoffs) {
+    // TODO calculate initial offset based on current date
+    return 3;
+  }
+
+  static gamesExcerptResponseToState(prevState, games, offset) {
+    const updatedGames = prevState.games.slice(0);
+    games.forEach((game, ix) => {
+      if (!updatedGames[offset + ix]) {
+        updatedGames[offset + ix] = game;
+      }
+    });
+    return { games: updatedGames };
+  }
+
   constructor(props) {
     super(props);
 
     this.state = {
+      bets: [],
       games: [],
+      kickoffs: [], // chronological list of game kickoffs only
 
       loadingError: false,
 
-      currentSliderOffset: 0,
-      loadedGamesUntilOffset: 0,
-      gamesDisplayed: 1,
+      currentOffset: 0,
+      gamesToDisplay: 3,
     };
 
     this.loadAhead = 5;
@@ -25,50 +45,72 @@ class CurrentGames extends Component {
   }
 
   componentDidMount() {
-    // TODO handle pagination params properly
-    this.fetchData(`${API_BASE_URL}/rtg/games/?offset=0&limit=5&from=${encodeURIComponent(format(this.fromDate))}`, 'games', true);
-    this.fetchData(`${API_BASE_URL}/rtg/bets/`, 'bets', false);
+    this.fetchData(`${API_BASE_URL}/rtg/bets/`, 'bets', false, (prevState, response) => ({ bets: response }));
+
+    fetch(`${API_BASE_URL}/rtg/game-kickoffs/`, {
+      headers: { Authorization: `Token ${AuthService.getToken()}` },
+    }).then(FetchHelper.parseJson)
+      .then((response) => {
+        if (response.ok) {
+          const kickoffs = response.json;
+          const initialOffset = CurrentGames.getInitialOffsetBasedOnDate(kickoffs);
+          this.setState(() => {
+            return {
+              kickoffs,
+              games: Array(kickoffs.length),
+              currentOffset: initialOffset,
+            };
+          }, () => {
+            const gamesUrl = `${API_BASE_URL}/rtg/games/?offset=${initialOffset}&limit=${this.state.gamesToDisplay}`;
+            this.fetchData(gamesUrl, 'games', true, (prevState, games) =>
+              CurrentGames.gamesExcerptResponseToState(prevState, games, initialOffset));
+          });
+        } else {
+          this.setState(() => ({ loadingError: true }));
+        }
+      }).catch(() => this.setState({ loadingError: true }));
   }
 
-  fetchData(url, targetStateField, isPaginated) {
+  fetchData(url, targetStateField, isPaginated, successResponseToStateMapper) {
     return fetch(url, {
       headers: { Authorization: `Token ${AuthService.getToken()}` },
     }).then(FetchHelper.parseJson)
       .then((response) => {
-        this.setState(() => (
+        this.setState(prevState => (
           response.ok
-            ? { [targetStateField]: isPaginated ? response.json.results : response.json }
+            ? { ...successResponseToStateMapper(prevState, isPaginated ? response.json.results : response.json) }
             : { loadingError: true }
         ));
       }).catch(() => this.setState({ loadingError: true }));
   }
 
   render() {
+    const gamesToDisplayWindow = CurrentGames
+      .range(this.state.currentOffset, this.state.currentOffset + this.state.gamesToDisplay);
     return (
-      <section className="CurrentGames">
-        {this.state.games.map(game => {
-          // TODO also load bets
-          // const userBet = bets.find(bet => bet.bettable === game.id) || {};
-          const userBet = {
-            result_bet_type: '',
-            points: 0,
-            result_bet: null,
-          };
-          return (
-            <GameCard key={game.id} userBet={userBet} {...game}>
-              <GameCardGameInfo
-                city={game.city}
-                kickoff={parse(game.kickoff)}
-                result={game.homegoals !== -1 && game.awaygoals !== -1 ? `${game.homegoals} : ${game.awaygoals}` : null}
-                resultBetType={userBet.result_bet_type}
-                points={userBet.points}
-                userBet={userBet.result_bet}
-              />
-            </GameCard>
-          );
-        })}
-      </section>
-    );
+      this.state.games ? (
+        <section className="CurrentGames">
+          {gamesToDisplayWindow.map(offset => {
+            // TODO also load bets
+            const game = this.state.games[offset];
+            if (game) {
+              const userBet = this.state.bets.find(bet => bet.bettable === game.id) || {};
+              return (
+                <GameCard key={game.id} userBet={userBet} {...game}>
+                  <GameCardGameInfo
+                    city={game.city}
+                    kickoff={parse(game.kickoff)}
+                    result={game.homegoals !== -1 && game.awaygoals !== -1 ? `${game.homegoals} : ${game.awaygoals}` : null}
+                    resultBetType={userBet.result_bet_type}
+                    points={userBet.points}
+                    userBet={userBet.result_bet}
+                  />
+                </GameCard>
+              );
+            }
+            return null;
+          })}
+        </section>) : null);
   }
 }
 
