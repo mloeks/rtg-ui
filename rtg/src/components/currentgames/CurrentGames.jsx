b6b5fb25f2ca178b925a4625e2e0ca06c072e3c0
matze09 <matthias@loeks.net>
2018-05-29 22:04:28 +0200

    First version of scrollable current games list
    Scroll button display and breakpoint handling.
    
    Not styled yet.
    No correct fetching of further games on scroll yet.

--- rtg/src/components/currentgames/CurrentGames.jsx
+++ rtg/src/components/currentgames/CurrentGames.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { parse } from 'date-fns';
+import { IconButton } from 'material-ui';
+import HardwareKeyboardArrowLeft from 'material-ui/svg-icons/hardware/keyboard-arrow-left';
+import HardwareKeyboardArrowRight from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
+import { viewportW } from 'verge';
 import FetchHelper from '../../service/FetchHelper';
 import AuthService, { API_BASE_URL } from '../../service/AuthService';
 import GameCard from '../GameCard';
@@ -12,7 +16,7 @@ class CurrentGames extends Component {
 
   static getInitialOffsetBasedOnDate(kickoffs) {
     // TODO calculate initial offset based on current date
-    return 3;
+    return 12;
   }
 
   static gamesExcerptResponseToState(prevState, games, offset) {
@@ -25,63 +29,114 @@ class CurrentGames extends Component {
     return { games: updatedGames };
   }
 
+  static getGamesToDisplay() {
+    const vw = viewportW();
+    if (vw < 640) { return 1; }
+    if (vw < 1280) { return 2; }
+    return 3;
+  }
+
   constructor(props) {
     super(props);
 
     this.state = {
       bets: [],
       games: [],
-      kickoffs: [], // chronological list of game kickoffs only
 
       loadingError: false,
 
       currentOffset: 0,
-      gamesToDisplay: 3,
+      gamesToDisplay: CurrentGames.getGamesToDisplay(),
     };
 
-    this.loadAhead = 5;
-    this.fromDate = new Date();
-    // this.fromDate = parse('2018-06-24T14:00:00+02:00');
+    this.mediaQueryList = [
+      window.matchMedia('(max-width: 640px)'),
+      window.matchMedia('(max-width: 1280px)'),
+    ];
+    this.loadGamesAroundView = 3;
+
+    this.onBreakpointChange = this.onBreakpointChange.bind(this);
+    this.fetchMoreGamesIfRequired = this.fetchMoreGamesIfRequired.bind(this);
+    this.canScrollForward = this.canScrollForward.bind(this);
+    this.canScrollBackward = this.canScrollBackward.bind(this);
+    this.scrollForward = this.scrollForward.bind(this);
+    this.scrollBackward = this.scrollBackward.bind(this);
   }
 
   componentDidMount() {
     this.fetchData(`${API_BASE_URL}/rtg/bets/`, 'bets', false, (prevState, response) => ({ bets: response }));
+    this.fetchKickoffs();
 
-    fetch(`${API_BASE_URL}/rtg/game-kickoffs/`, {
-      headers: { Authorization: `Token ${AuthService.getToken()}` },
-    }).then(FetchHelper.parseJson)
-      .then((response) => {
-        if (response.ok) {
-          const kickoffs = response.json;
-          const initialOffset = CurrentGames.getInitialOffsetBasedOnDate(kickoffs);
-          this.setState(() => {
-            return {
-              kickoffs,
-              games: Array(kickoffs.length),
-              currentOffset: initialOffset,
-            };
-          }, () => {
-            const gamesUrl = `${API_BASE_URL}/rtg/games/?offset=${initialOffset}&limit=${this.state.gamesToDisplay}`;
-            this.fetchData(gamesUrl, 'games', true, (prevState, games) =>
-              CurrentGames.gamesExcerptResponseToState(prevState, games, initialOffset));
-          });
-        } else {
-          this.setState(() => ({ loadingError: true }));
-        }
-      }).catch(() => this.setState({ loadingError: true }));
+    this.mediaQueryList.forEach(mql => mql.addListener(this.onBreakpointChange));
   }
 
-  fetchData(url, targetStateField, isPaginated, successResponseToStateMapper) {
+  componentWillUnmount() {
+    this.mediaQueryList.forEach(mql => mql.removeEventListener(this.onBreakpointChange));
+  }
+
+  onBreakpointChange() {
+    this.setState({ gamesToDisplay: CurrentGames.getGamesToDisplay() });
+  }
+
+  fetchData(url, targetStateField, isPaginated, responseToStateMapper) {
     return fetch(url, {
       headers: { Authorization: `Token ${AuthService.getToken()}` },
-    }).then(FetchHelper.parseJson)
-      .then((response) => {
-        this.setState(prevState => (
-          response.ok
-            ? { ...successResponseToStateMapper(prevState, isPaginated ? response.json.results : response.json) }
-            : { loadingError: true }
-        ));
-      }).catch(() => this.setState({ loadingError: true }));
+    }).then(FetchHelper.parseJson).then((response) => {
+      this.setState(prevState => (response.ok ?
+        { ...responseToStateMapper(prevState, isPaginated ? response.json.results : response.json) }
+        : { loadingError: true }
+      ));
+    }).catch(() => this.setState({ loadingError: true }));
+  }
+
+  fetchGames(offset) {
+    const gamesUrl = `${API_BASE_URL}/rtg/games/?offset=${offset}&limit=${this.state.gamesToDisplay}`;
+    this.fetchData(gamesUrl, 'games', true, (prevState, games) =>
+      CurrentGames.gamesExcerptResponseToState(prevState, games, offset));
+  }
+
+  fetchKickoffs() {
+    fetch(`${API_BASE_URL}/rtg/game-kickoffs/`, {
+      headers: { Authorization: `Token ${AuthService.getToken()}` },
+    }).then(FetchHelper.parseJson).then((response) => {
+      if (response.ok) {
+        const kickoffs = response.json;
+        const initialOffset = CurrentGames.getInitialOffsetBasedOnDate(kickoffs);
+        this.setState(() => ({
+          games: Array(kickoffs.length),
+          currentOffset: initialOffset,
+        }), () => { this.fetchGames(initialOffset); });
+      } else {
+        this.setState(() => ({ loadingError: true }));
+      }
+    }).catch(() => this.setState({ loadingError: true }));
+  }
+
+  fetchMoreGamesIfRequired() {
+    // TODO find out if more games need to be loaded based on window + gamesAround and trigger the request
+    console.log(this.state.games);
+    console.log(this.state.currentOffset);
+  }
+
+  canScrollForward() {
+    return this.state.currentOffset + this.state.gamesToDisplay < this.state.games.length;
+  }
+
+  canScrollBackward() { return this.state.currentOffset > 0; }
+
+  scrollForward() {
+    this.setState((prevState) => {
+      const nextOffset = prevState.currentOffset + prevState.gamesToDisplay;
+      const maxOffset = prevState.games.length - prevState.gamesToDisplay;
+      return { currentOffset: Math.min(nextOffset, maxOffset) };
+    }, this.fetchMoreGamesIfRequired);
+  }
+
+  scrollBackward() {
+    this.setState((prevState) => {
+      const nextOffset = prevState.currentOffset - prevState.gamesToDisplay;
+      return { currentOffset: Math.max(nextOffset, 0) };
+    }, this.fetchMoreGamesIfRequired);
   }
 
   render() {
@@ -90,8 +145,13 @@ class CurrentGames extends Component {
     return (
       this.state.games ? (
         <section className="CurrentGames">
-          {gamesToDisplayWindow.map(offset => {
-            // TODO also load bets
+          {this.canScrollBackward() &&
+            <IconButton onClick={this.scrollBackward}><HardwareKeyboardArrowLeft /></IconButton>}
+          {this.canScrollForward() &&
+            <IconButton onClick={this.scrollForward}><HardwareKeyboardArrowRight /></IconButton>}
+
+
+          {gamesToDisplayWindow.map((offset) => {
             const game = this.state.games[offset];
             if (game) {
               const userBet = this.state.bets.find(bet => bet.bettable === game.id) || {};
@@ -108,7 +168,8 @@ class CurrentGames extends Component {
                 </GameCard>
               );
             }
-            return null;
+            // TODO style loading state
+            return <div key={`empty-game-${offset}`}>--- LEER ---</div>;
           })}
         </section>) : null);
   }
