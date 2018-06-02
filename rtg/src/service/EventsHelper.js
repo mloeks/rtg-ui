85b83bf870e20580748523f127badfaabef81cef
matze09 <matthias@loeks.net>
2018-06-02 23:03:03 +0200

    Add throttle function from underscore.js

--- rtg/src/service/EventsHelper.js
+++ rtg/src/service/EventsHelper.js
@@ -119,3 +119,47 @@ export function debounce(func, wait, immediate) {
   };
 }
 /* eslint-enable */
+
+// Taken from underscore.js: http://underscorejs.org/docs/underscore.html
+/* eslint-disable */
+export function throttle(func, wait, options) {
+  var timeout, context, args, result;
+  var previous = 0;
+  if (!options) options = {};
+
+  var later = function() {
+    previous = options.leading === false ? 0 : new Date();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+
+  var throttled = function() {
+    var now = new Date();
+    if (!previous && options.leading === false) previous = now;
+    var remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
+
+  throttled.cancel = function() {
+    clearTimeout(timeout);
+    previous = 0;
+    timeout = context = args = null;
+  };
+
+  return throttled;
+}
+/* eslint-enable */
