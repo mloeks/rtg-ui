154fb05e8a2f6be49d3d13611888e87114eb7740
matze09 <matthias@loeks.net>
2018-06-03 09:49:40 +0200

    Allow img not to be set - then a skeleton placeholder is rendered

--- rtg/src/components/BigPicture.jsx
+++ rtg/src/components/BigPicture.jsx
@@ -3,23 +3,28 @@ import PropTypes from 'prop-types';
 
 import './BigPicture.css';
 
-const BigPicture = props => (
-  <section
-    className="BigPicture"
-    style={{ backgroundImage: `url(${props.img})`, backgroundPositionY: `${props.positionY}%` }}
-  >
-    {props.children}
-  </section>
-);
+const BigPicture = (props) => {
+  const style = props.img ? {
+    backgroundImage: `url(${props.img})`,
+    backgroundPositionY: `${props.positionY}%`,
+  } : {};
+
+  return (
+    <section className={`BigPicture ${!props.img ? 'BigPicture--skeleton' : ''}`} style={style} >
+      {props.children}
+    </section>
+  );
+};
 
 BigPicture.propTypes = {
   children: PropTypes.node,
-  img: PropTypes.string.isRequired,
+  img: PropTypes.string,
   positionY: PropTypes.number,
 };
 
 BigPicture.defaultProps = {
   children: null,
+  img: null,
   positionY: 50,
 };
 
