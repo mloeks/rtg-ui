3fa32b1005f2a5790a52874fd62a6385b0f279a8
matze09 <matthias@loeks.net>
2018-06-06 23:45:49 +0200

    Add lazy loading functionality to BigPicture component
    It now can be configured to only load the image when it's about
    to be in the viewport of the user.
    This property is false by default and should be used sparsely,
    since it needs to register a scroll and resize listener on each
    corresponding image.
    This will be useful especially for the Foyer page, where a lot
    of the initial loading footprint can be saved by lazily loading
    the pictures which are not initially in view.

--- rtg/src/components/BigPicture.jsx
+++ rtg/src/components/BigPicture.jsx
@@ -1,31 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { inViewport } from 'verge';
 import PropTypes from 'prop-types';
+import { ThrottledEventListener, ThrottledScrollPositionListener } from '../service/EventsHelper';
 
 import './BigPicture.css';
 
-const BigPicture = (props) => {
-  const style = props.img ? {
-    backgroundImage: `url(${props.img})`,
-    backgroundPositionY: `${props.positionY}%`,
-  } : {};
+class BigPicture extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <section className={`BigPicture ${!props.img ? 'BigPicture--skeleton' : ''}`} style={style} >
-      {props.children}
-    </section>
-  );
+    this.state = {
+      showImg: !this.props.lazyLoadWhenInViewport,
+    };
+
+    this.ref = React.createRef();
+    if (this.props.lazyLoadWhenInViewport) {
+      this.scrollListener = new ThrottledScrollPositionListener();
+      this.resizeListener = new ThrottledEventListener('resize');
+    }
+    this.displayIfLazyLoadAndInView = this.displayIfLazyLoadAndInView.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.lazyLoadWhenInViewport) {
+      this.scrollListener.addCallback(this.displayIfLazyLoadAndInView);
+      this.resizeListener.addCallback(this.displayIfLazyLoadAndInView);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.scrollListener) this.scrollListener.removeAll();
+    if (this.resizeListener) this.resizeListener.removeAll();
+  }
+
+  displayIfLazyLoadAndInView() {
+    if (this.props.lazyLoadWhenInViewport && inViewport(this.ref.current, -100)) {
+      this.setState({ showImg: true });
+    }
+  }
+
+  render() {
+    const style = this.state.showImg ? {
+      backgroundImage: `url(${this.props.img})`,
+      backgroundPositionY: `${this.props.positionY}%`,
+    } : {};
+
+    return (
+      <section ref={this.ref} className={`BigPicture ${!this.state.showImg ? 'BigPicture--skeleton' : ''}`} style={style}>
+        {this.props.children}
+      </section>
+    );
+  }
+}
+
+BigPicture.defaultProps = {
+  children: null,
+  lazyLoadWhenInViewport: false,
+  positionY: 50,
 };
 
 BigPicture.propTypes = {
   children: PropTypes.node,
-  img: PropTypes.string,
+  img: PropTypes.string.isRequired,
+  lazyLoadWhenInViewport: PropTypes.bool,
   positionY: PropTypes.number,
-};
-
-BigPicture.defaultProps = {
-  children: null,
-  img: null,
-  positionY: 50,
 };
 
 export default BigPicture;
