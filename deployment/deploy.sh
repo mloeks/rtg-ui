265235ea558448b95e276ce764bc525bba3ae4f3
matze09 <matthias@loeks.net>
2018-06-10 22:17:15 +0200

    Adjust deploy script to updated node version

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,13 +6,13 @@ SSH=muden@s17.wservices.ch
 cd ../rtg
 
 echo "Preparing NPM..."
-/home/mloeks/.nvm/versions/node/v8.9.4/bin/npm run clean
-#/home/mloeks/.nvm/versions/node/v8.9.4/bin/npm i
-#/home/mloeks/.nvm/versions/node/v8.9.4/bin/npm rebuild node-sass --force
+/home/mloeks/.nvm/versions/node/v8.11.2/bin/npm run clean
+#/home/mloeks/.nvm/versions/node/v8.11.2/bin/npm i
+#/home/mloeks/.nvm/versions/node/v8.11.2/bin/npm rebuild node-sass --force
 
 echo "Building ${ENV} assets..."
 NPM_SCRIPT=$([ "$ENV" == "PROD" ] && echo "build" || echo "build:demo")
-/home/mloeks/.nvm/versions/node/v8.9.4/bin/npm run ${NPM_SCRIPT}
+/home/mloeks/.nvm/versions/node/v8.11.2/bin/npm run ${NPM_SCRIPT}
 
 echo "Uploading new assets..."
 TARGET_APP=$([ "$ENV" == "PROD" ] && echo "rtg" || echo "rtg_demo")
