b4b1165830fc525e6f399759d4745bd7ed0bd8f5
matze09 <matthias@loeks.net>
2018-10-07 19:55:33 +0200

    Material UI 1.x upgrade: OpenBetsIndicator styling

--- rtg/src/components/header/OpenBetsIndicator.jsx
+++ rtg/src/components/header/OpenBetsIndicator.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { withTheme } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
-import { error } from '../../theme/RtgTheme';
 
-const OpenBetsIndicator = props => (
-  props.number > 0 ?
-    <Link to="/bets" style={{ textDecoration: 'none' }} title="Tipps abgeben">
-      <Avatar
-        color="#FFF"
-        backgroundColor={error}
-        size={24}
-        style={{ fontWeight: '500' }}
-      >{props.number}
-      </Avatar>
-    </Link>
-    : null
+const OpenBetsIndicator = ({ number, theme }) => (
+  number > 0
+    ? (
+      <Link to="/bets" style={{ textDecoration: 'none' }} title="Tipps abgeben">
+        <Avatar
+          style={{
+            width: 24,
+            height: 24,
+            color: theme.palette.common.white,
+            backgroundColor: theme.palette.error.main,
+            fontSize: 12,
+            fontWeight: '500',
+          }}
+        >
+          {number}
+        </Avatar>
+      </Link>
+    ) : null
 );
 
 OpenBetsIndicator.propTypes = {
   number: PropTypes.number.isRequired,
+  theme: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
 };
 
-export default OpenBetsIndicator;
+export default withTheme()(OpenBetsIndicator);
