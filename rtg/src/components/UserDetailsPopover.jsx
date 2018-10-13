98d21522185565b6a02337fa36d5b927233f6867
matze09 <matthias@loeks.net>
2018-10-14 00:21:24 +0200

    Material UI 1.x upgrade: UserDetailsPopover behaviour and UI
    Not yet complete. The close callback does not yet work properly.

--- rtg/src/components/UserDetailsPopover.jsx
+++ rtg/src/components/UserDetailsPopover.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { withTheme } from '@material-ui/core/styles';
+import { withStyles, withTheme } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import IconButton from '@material-ui/core/IconButton';
 import Popover from '@material-ui/core/Popover';
@@ -13,6 +13,12 @@ import FetchHelper from '../service/FetchHelper';
 import AuthService, { API_BASE_URL } from '../service/AuthService';
 
 import './UserDetailsPopover.css';
+
+const styles = {
+  paper: {
+    overflow: 'visible',
+  },
+};
 
 // TODO P3 also load statistics and offer to show rank, bet stats etc. to other users
 class UserDetailsPopover extends Component {
@@ -47,22 +53,23 @@ class UserDetailsPopover extends Component {
       anchorEl,
       anchorOrigin,
       avatar,
+      classes,
       onClose,
       open,
-      targetOrigin,
+      transformOrigin,
       theme,
       username,
     } = this.props;
 
     return (
       <Popover
-        animated={false}
         anchorEl={anchorEl}
         anchorOrigin={anchorOrigin}
-        onRequestClose={onClose}
+        classes={{ paper: classes.paper }}
+        onClose={onClose}
         open={open}
-        targetOrigin={targetOrigin}
-        style={{ backgroundColor: 'transparent', boxShadow: 'none' }}
+        transformOrigin={transformOrigin}
+        style={{ backgroundColor: 'transparent', boxShadow: 'none', marginBottom: 10 }}
       >
         <div className="UserDetailsPopover">
           <div className="UserDetailsPopover__content">
@@ -86,7 +93,7 @@ class UserDetailsPopover extends Component {
               <CloseIcon style={{ color: theme.palette.grey['900'], width: 18, height: 18 }} />
             </IconButton>
 
-            <h3 className="UserDetailsPopover__username">{this.props.username}</h3>
+            <h3 className="UserDetailsPopover__username">{username}</h3>
 
             {detailsLoading && <CircularProgress />}
 
@@ -100,10 +107,11 @@ class UserDetailsPopover extends Component {
                   </p>
                 )}
                 {user.location && (
-                  <p className="UserDetailsPopover__location">
-                    <PlaceIcon
-                      style={{ color: theme.palette.grey['400'], width: 18, height: 18 }}
-                    />
+                  <p
+                    className="UserDetailsPopover__location"
+                    style={{ color: theme.palette.grey['600'] }}
+                  >
+                    <PlaceIcon color="inherit" style={{ width: 18, height: 18 }} />
                     &nbsp;
                     {user.location}
                   </p>
@@ -121,7 +129,7 @@ UserDetailsPopover.defaultProps = {
   anchorEl: null,
   avatar: null,
   anchorOrigin: { horizontal: 'left', vertical: 'top' },
-  targetOrigin: { horizontal: 'left', vertical: 'bottom' },
+  transformOrigin: { horizontal: 'left', vertical: 'bottom' },
 };
 
 UserDetailsPopover.propTypes = {
@@ -130,7 +138,7 @@ UserDetailsPopover.propTypes = {
     horizontal: PropTypes.string.isRequired,
     vertical: PropTypes.string.isRequired,
   }),
-  targetOrigin: PropTypes.shape({
+  transformOrigin: PropTypes.shape({
     horizontal: PropTypes.string.isRequired,
     vertical: PropTypes.string.isRequired,
   }),
@@ -141,7 +149,10 @@ UserDetailsPopover.propTypes = {
 
   onClose: PropTypes.func.isRequired,
 
-  theme: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  /* eslint-disable react/forbid-prop-types */
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+  /* eslint-enable react/forbid-prop-types */
 };
 
-export default withTheme()(UserDetailsPopover);
+export default withStyles(styles)(withTheme()(UserDetailsPopover));
