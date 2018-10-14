52d89d19c95a49ee9fda6fd436029bb7eb3057ec
matze09 <matthias@loeks.net>
2018-10-14 23:59:24 +0200

    Material UI 1.x upgrade: UsersGridToolbar
    New style filter menu with switches and icons.
    Use input adornments for easier implementation of search input
    field.

--- rtg/src/components/admin/UsersGridToolbar.jsx
+++ rtg/src/components/admin/UsersGridToolbar.jsx
@@ -1,92 +1,128 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { withTheme } from '@material-ui/core/styles';
+import { withStyles, withTheme } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
+import FormControl from '@material-ui/core/FormControl';
 import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import InputLabel from '@material-ui/core/InputLabel';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
+import Switch from '@material-ui/core/Switch';
 import Toolbar from '@material-ui/core/Toolbar';
 
-import FilterListIcon from '@material-ui/icons/FilterList';
-import SearchIcon from '@material-ui/icons/Search';
 import BackspaceIcon from '@material-ui/icons/Backspace';
+import EuroSymbolIcon from '@material-ui/icons/EuroSymbol';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import PersonIcon from '@material-ui/icons/Person';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import SearchIcon from '@material-ui/icons/Search';
 import teal from '@material-ui/core/colors/teal';
 
-const UsersGridToolbar = ({
-  filterActive, filterHasNotPaid, onFilterHasNotPaidToggled, filterInactive,
-  onFilterActiveToggled, onFilterInactiveToggled, onSearchTermUpdated, searchTerm, theme,
-}) => (
-  <Toolbar
-    className="UsersGridToolbar"
-    style={{
-      position: 'sticky',
-      top: 0,
-      backgroundColor: teal['200'],
-      height: '60px',
-      boxShadow: '0 3px 5px rgba(57, 63, 72, 0.3)',
-      zIndex: 100,
-      transition: 'top 200ms cubic-bezier(0.0, 0.0, 0.2, 1)',
-    }}
-  >
-    <Menu
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-      clickCloseDelay={0}
-      iconButtonElement={(
-        <IconButton>
+const styles = {
+  filterMenu: {
+    minWidth: 300,
+  },
+};
+
+class UsersGridToolbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { filterMenuAnchorEl: null };
+  }
+
+  render() {
+    const { filterMenuAnchorEl } = this.state;
+    const {
+      classes, filterActive, filterHasNotPaid, onFilterHasNotPaidToggled, filterInactive,
+      onFilterActiveToggled, onFilterInactiveToggled, onSearchTermUpdated, searchTerm, theme,
+    } = this.props;
+
+    return (
+      <Toolbar
+        className="UsersGridToolbar"
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          position: 'sticky',
+          top: 0,
+          backgroundColor: teal['200'],
+          height: 60,
+          boxShadow: '0 3px 5px rgba(57, 63, 72, 0.3)',
+          zIndex: 100,
+          transition: 'top 200ms cubic-bezier(0.0, 0.0, 0.2, 1)',
+        }}
+      >
+        <IconButton
+          aria-label="Filter"
+          onClick={e => this.setState({ filterMenuAnchorEl: e.currentTarget })}
+        >
           <FilterListIcon style={{ color: theme.palette.grey['700'] }} />
         </IconButton>
-      )}
-    >
-      <MenuItem
-        primaryText="Nur aktive User"
-        checked={filterActive}
-        insetChildren
-        onClick={onFilterActiveToggled}
-        style={{ textAlign: 'left' }}
-      />
-      <MenuItem
-        primaryText="Nur inaktive User"
-        checked={filterInactive}
-        insetChildren
-        onClick={onFilterInactiveToggled}
-        style={{ textAlign: 'left' }}
-      />
-      <Divider />
-      <MenuItem
-        primaryText="Tippeinsatz unbezahlt"
-        checked={filterHasNotPaid}
-        insetChildren
-        onClick={onFilterHasNotPaidToggled}
-        style={{ textAlign: 'left' }}
-      />
-    </Menu>
-    <SearchIcon style={{
-      position: 'absolute',
-      left: 0,
-      bottom: '20px',
-      width: 20,
-      height: 20,
-      color: theme.palette.grey['500'],
-    }}
-    />
-    <TextField
-      hintText="User suchen"
-      value={searchTerm}
-      onChange={(e, v) => onSearchTermUpdated(v)}
-      hintStyle={{ padding: '0 25px' }}
-      inputStyle={{ padding: '0 25px', width: 'auto' }}
-    />
-    <IconButton
-      iconStyle={{ width: 20, height: 20 }}
-      style={{ position: 'absolute', right: 0, bottom: '6px' }}
-      onClick={() => onSearchTermUpdated('')}
-    >
-      <BackspaceIcon style={{ color: theme.palette.grey['700'] }} />
-    </IconButton>
-  </Toolbar>
-);
+        <Menu
+          anchorEl={filterMenuAnchorEl}
+          open={Boolean(filterMenuAnchorEl)}
+          onClose={() => this.setState({ filterMenuAnchorEl: null })}
+          classes={{ paper: classes.filterMenu }}
+        >
+          <ListItem>
+            <ListItemIcon><PersonIcon /></ListItemIcon>
+            <ListItemText primary="Nur aktive User" />
+            <ListItemSecondaryAction>
+              <Switch onChange={onFilterActiveToggled} checked={filterActive} />
+            </ListItemSecondaryAction>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon><PersonOutlineIcon /></ListItemIcon>
+            <ListItemText primary="Nur inaktive User" />
+            <ListItemSecondaryAction>
+              <Switch onChange={onFilterInactiveToggled} checked={filterInactive} />
+            </ListItemSecondaryAction>
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemIcon><EuroSymbolIcon /></ListItemIcon>
+            <ListItemText primary="Tippeinsatz unbezahlt" />
+            <ListItemSecondaryAction>
+              <Switch onChange={onFilterHasNotPaidToggled} checked={filterHasNotPaid} />
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Menu>
+
+        <FormControl>
+          <InputLabel>User suchen</InputLabel>
+          <Input
+            value={searchTerm}
+            onChange={e => onSearchTermUpdated(e.target.value)}
+            startAdornment={(
+              <InputAdornment position="start">
+                <SearchIcon style={{ color: theme.palette.grey['600'] }} />
+              </InputAdornment>
+            )}
+            endAdornment={(
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="Suchbegriff löschen"
+                  onClick={() => onSearchTermUpdated('')}
+                  onMouseDown={(e) => {
+                    e.preventDefault();
+                  }}
+                >
+                  <BackspaceIcon style={{ color: theme.palette.grey['600'] }} />
+                </IconButton>
+              </InputAdornment>
+            )}
+          />
+        </FormControl>
+      </Toolbar>
+    );
+  }
+}
 
 UsersGridToolbar.defaultProps = {
   filterActive: false,
@@ -106,7 +142,10 @@ UsersGridToolbar.propTypes = {
   onFilterHasNotPaidToggled: PropTypes.func.isRequired,
   onSearchTermUpdated: PropTypes.func.isRequired,
 
-  theme: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  /* eslint-disable react/forbid-prop-types */
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+  /* eslint-enable react/forbid-prop-types */
 };
 
-export default withTheme()(UsersGridToolbar);
+export default withStyles(styles)(withTheme()(UsersGridToolbar));
