74c702e749831d7500d06acb1df6497f78b55653
mloeks <matthias@loeks.net>
2024-05-07 17:58:29 +0200

    Fix country flag importer with CRA v5

--- rtg/src/theme/CountryFlagImporter.js
+++ rtg/src/theme/CountryFlagImporter.js
@@ -4,7 +4,7 @@ const obj = {};
 context.keys().forEach((key) => {
   const countryCode = key.split('./').pop() // remove the first 2 characters
     .substring(0, key.length - 6); // remove the file extension
-  obj[countryCode] = context(key).default;
+  obj[countryCode] = context(key);
 });
 
 export default obj;
